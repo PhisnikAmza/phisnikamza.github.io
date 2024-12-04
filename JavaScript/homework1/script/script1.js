8057ecb86e1c4120eec3b9b43b5e2e88fb26dd43
PhisnikAmza <fisnikvamza@gmail.com>
2024-12-04 20:55:02 +0100

    homework1

---
+++ JavaScript/homework1/script/script1.js
@@ -0,0 +1,17 @@
+// alert("hello")
+
+
+let phoneprice = 119.95;
+let tax = 0.05;
+
+
+
+let totalphone = phoneprice * 30;
+
+
+
+
+let pricewithtax = totalphone * tax;
+
+console.log(pricewithtax);
+
