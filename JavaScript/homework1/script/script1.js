d559305e1caba09c3565d09e3bf0ae40516fb196
PhisnikAmza <fisnikvamza@gmail.com>
2024-12-04 21:04:51 +0100

    js

--- JavaScript/homework1/script/script1.js
+++ JavaScript/homework1/script/script1.js
@@ -4,14 +4,8 @@
 let phoneprice = 119.95;
 let tax = 0.05;
 
-
-
 let totalphone = phoneprice * 30;
-
-
-
-
 let pricewithtax = totalphone * tax;
 
-console.log(pricewithtax);
+console.log("The price of all phones with tax is : ", pricewithtax);
 
